b5e0aacfb84b867ad2a64d3e30d7e03dc34d950c
sweetbabyjay <jayfly64@gmail.com>
2021-01-05 11:56:31 -0800

    Added the api-routes for notes

---
+++ Develop/routes/api-routes.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+let db = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf-8');
+db = JSON.parse(db)
+
+function apiRoutes(app) {
+    app.get('/api/notes/:id', function (req, res) {
+        res.json(db);
+    });
+
+    app.get('/api/notes.:id', function (req, res) {
+        res.json(data[Number(req.params.id)]);
+    });
+
+    app.post('/api/notes', function (req, res) {
+        const newNotes = req.body;
+        let notesId = newNotes.title + Math.floor(Math.random() * 10);
+        newNotes.id = notesId;
+        db.push(newNotes);
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), function (err) {
+            res.json(db);
+        });
+    });
+
+    app.delete('/api/notes.:id', function (req, res) {
+        const removeNote = req.params.id;
+        let savedNotes = [];
+        for (let i = 0; i < db.length; i++) {
+            if (db[i].id !== removeNote) {
+                savedNotes.push(db[i])
+            }
+        }
+
+        db = [...savedNotes]
+
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(savedNotes), function (err) {
+            if (err) throw (err);
+            res.send('Deleted Notes!');
+        })
+    })
+}
